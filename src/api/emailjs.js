19cac9093517698d79f76a7fae7d8cf5d6542066
gloria <gloria.hh.gwak@gmail.com>
2025-01-20 18:12:05 +1100

    added emailjs config

--- src/api/emailjs.js
+++ src/api/emailjs.js
@@ -1,6 +1,19 @@
 
 import emailjs from '@emailjs/browser';
 
+emailjs.init({
+    publicKey: process.env.REACT_APP_EMAILJS_PUBLIC_KEY,
+    blockHeadless: true,
+    // blockList: {
+    //   list: ['foo@emailjs.com', 'bar@emailjs.com'], // 차단할 이메일
+    //   watchVariable: 'userEmail', // 감시할 변수
+    // },
+    limitRate: {
+        id: 'app',
+        throttle: 10000, // request once per one sendond
+    },
+});
+
 export async function emailSend(inquirie, userTime){
     const templateParams = {
         name: inquirie.name,
@@ -15,7 +28,8 @@ export async function emailSend(inquirie, userTime){
         templateParams
     ).then(
         (response) => {
-            console.log('SUCCESS!', response.status, response.text,templateParams);
+            // console.log('SUCCESS!', response.status, response.text,templateParams);
+            console.log('SUCCESS!');
         },
         (error) => {
         console.log('FAILED...', error);
